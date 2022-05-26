48e32260a31ef1c2d6eea523ee5c95ca65420365
Sofiaevx <villanueva19.sofia@gmail.com>
2022-05-26 21:12:31 +0800

    Created my temporary design and labeling all the buttons according to their positions

---
+++ Tic Tac Toe/Tic Tac Toe/TicTacToe.vb
@@ -0,0 +1,26 @@
+﻿Public Class TicTacToe
+    Dim turn As Boolean = True
+
+
+    Private Sub btn1to9(sender As Object, e As EventArgs) Handles Button9.Click, Button8.Click, Button7.Click, Button6.Click, Button5.Click, Button4.Click, Button3.Click, Button2.Click, Button1.Click
+        Dim x As Button = CType(sender, Button)
+
+        If turn Then
+
+            x.Image = System.Drawing.Image.FromFile("x.png")
+            x.BackColor = System.Drawing.Color.White
+            turn = False
+
+
+
+
+
+
+        Else
+            x.BackColor = System.Drawing.Color.White
+            x.Image = System.Drawing.Image.FromFile("o.png")
+            turn = True
+
+        End If
+    End Sub
+End Class
