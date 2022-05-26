7c1db56f8fd2c36197d538f3b59affd403266ca0
Sofiaevx <villanueva19.sofia@gmail.com>
2022-05-27 00:00:53 +0800

    I added in checker function a perform click event on reset button to reset the game if one of the players won

--- Tic Tac Toe/Tic Tac Toe/TicTacToe.vb
+++ Tic Tac Toe/Tic Tac Toe/TicTacToe.vb
@@ -25,84 +25,99 @@
         '1st Horizontal Result
         If TopLeftBtn.Text.Equals(MiddleTopBtn.Text) And MiddleTopBtn.Text.Equals(TopRightBtn.Text) And TopLeftBtn.Enabled = False Then
             If TopLeftBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
                 ResetBtn.PerformClick()
 
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
             '2nd Horizontal Result
         ElseIf MiddleLeftBtn.Text.Equals(MiddleCenterBtn.Text) And MiddleCenterBtn.Text.Equals(MiddleRightBtn.Text) And MiddleLeftBtn.Enabled = False Then
             If MiddleLeftBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
+                ResetBtn.PerformClick()
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
             '3rdHorizontal Result
         ElseIf BottomLeftBtn.Text.Equals(MiddleBottomBtn.Text) And MiddleBottomBtn.Text.Equals(BottomRightBtn.Text) And BottomLeftBtn.Enabled = False Then
             If BottomLeftBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
+                ResetBtn.PerformClick()
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
 
             '1st Vertical Result
         ElseIf TopLeftBtn.Text.Equals(MiddleLeftBtn.Text) And MiddleLeftBtn.Text.Equals(BottomLeftBtn.text) And TopLeftBtn.Enabled = False Then
             If TopLeftBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
+                ResetBtn.PerformClick()
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
             '2nd Vertical Result
         ElseIf MiddleTopBtn.Text.Equals(MiddleCenterBtn.Text) And MiddleCenterBtn.Text.Equals(MiddleBottomBtn.text) And MiddleTopBtn.Enabled = False Then
             If MiddleTopBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
+                ResetBtn.PerformClick()
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
             '3rd Vertical Result
         ElseIf TopRightBtn.Text.Equals(MiddleRightBtn.Text) And MiddleRightBtn.Text.Equals(BottomRightBtn.text) And TopRightBtn.Enabled = False Then
             If TopRightBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
+                ResetBtn.PerformClick()
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
             '1st Diagonal Result
         ElseIf TopLeftBtn.Text.Equals(MiddleCenterBtn.Text) And MiddleCenterBtn.Text.Equals(BottomRightBtn.Text) And TopLeftBtn.Enabled = False Then
             If TopLeftBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
+                ResetBtn.PerformClick()
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
             '2nd Diagonal Result
         ElseIf TopRightBtn.Text.Equals(MiddleCenterBtn.Text) And MiddleCenterBtn.Text.Equals(BottomLeftBtn.Text) And TopRightBtn.Enabled = False Then
             If TopRightBtn.Text.Equals("X") Then
-                playeronescore += 1
                 MessageBox.Show("Player 1 Wins!!")
+                playeronescore += 1
+                ResetBtn.PerformClick()
             Else
                 MessageBox.Show("Player 2 Wins!!")
                 playertwoscore += 1
+                ResetBtn.PerformClick()
             End If
 
         End If
